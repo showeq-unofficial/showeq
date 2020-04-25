a4d38f2a169488cc55c83a1aa06ea234f571acdb
cn187 <cn187@19a12193-7728-0410-8f54-e9f61ebc0c9f>
2020-04-25 17:11:52 +0000

    Fix packet decode delay with libpcap >= 1.9.1

--- src/packetcapture.cpp
+++ src/packetcapture.cpp
@@ -136,15 +136,19 @@ void PacketCaptureThread::start(const char *device, const char *host,
      ** Not all  platforms  support  a read timeout; on platforms that
      ** don't, the read timeout is ignored.
      ** 
-     ** In Linux 2.4.x with the to_ms set to 0 we get packets immediatly,
-     ** and thats what we need in this application, so don't change it!! 
+     ** In Linux 2.4.x with the to_ms set to 0 we got packets immediatly,
+     ** and thats what we need in this application.  However, as of libpcap
+     ** 1.9.1, a timeout of 0 means infinity, so that no longer works. A
+     ** negative timeout will use the default kernal timeout, which can
+     ** vary.  So the most prudent option is to set the timeout as low
+     ** as we can, to 1 ms.
      ** 
      ** a race condition exists between this thread and the main thread 
      ** any artificial delay in getting packets can cause filtering problems
      ** and cause us to miss new stream when the player zones.
      */
     // initialize the pcap object 
-    m_pcache_pcap = pcap_open_live((char *) device, BUFSIZ, true, 0, ebuf);
+    m_pcache_pcap = pcap_open_live((char *) device, BUFSIZ, true, 1, ebuf);
 #ifdef __FreeBSD__
     // if we're on FreeBSD, we need to call ioctl on the file descriptor
     // with BIOCIMMEDIATE to get the kernel Berkeley Packet Filter device
