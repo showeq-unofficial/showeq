8317cd862132dca08e9b83b2e3bbce40bce17a08
theblueadept <theblueadept@19a12193-7728-0410-8f54-e9f61ebc0c9f>
2022-11-17 23:24:30 +0000

    Historical checkin for 11/17/22 - Opcode and struct changes (Newby)
    Version to 6.2.12

--- src/everquest.h
+++ src/everquest.h
@@ -1112,23 +1112,26 @@ struct spawnStruct
          {
            struct
            {
-              unsigned heading:12;                        // heading
-              signed   deltaHeading:10;                   // change in heading
-              unsigned padding00:10;
-
-              signed   x:19;                              // x coord (1st loc value)
-              signed   animation:10;                      // current animation
-              unsigned padding01:3;
-
-              signed   z:19;                              // z coord (3rd loc value)
-              signed   deltaX:13;                         // change in x
-
-              signed   deltaZ:13;                         // change in z
-              signed   y:19;                              // y coord (2nd loc value)
 
               unsigned pitch:12;                          // pitch (up/down heading)
               signed   deltaY:13;                         // change in y
-              unsigned padding04:7;
+              unsigned padding00:7;
+
+              signed   z:19;                              // z coord (3rd loc value)
+              signed   animation:10;                      // current animation
+              unsigned padding01:3;
+
+              signed   x:19;                              // x coord (1st loc value)
+              signed   deltaHeading:10;                   // change in heading
+              unsigned padding02:3;
+
+              unsigned heading:12;                        // heading
+              signed   y:19;                              // y coord (2nd loc value)
+              unsigned padding03:1;
+
+              signed   deltaX:13;                         // change in x
+              signed   deltaZ:13;                         // change in z
+              unsigned padding04:6;
 
            };
            int32_t posData[5];
@@ -2388,29 +2391,31 @@ struct playerSpawnPosStruct
 /*0000*/ uint16_t spawnId;
 /*0002*/ uint16_t spawnId2;
 /*0004*/
-         unsigned heading:12;                      // heading
-         signed   deltaHeading:10;                 // change in heading
-         unsigned padding00:10;
+         unsigned pitch:12;                        // pitch (up/down heading)
+         signed   deltaY:13;                       // change in y
+         unsigned padding00:7;
 /*0008*/
-         signed   x:19;                            // x coord (1st loc value)
+         signed   z:19;                            // z coord (3rd loc value)
          signed   animation:10;                    // current animation
          unsigned padding01:3;
 /*0012*/
-         signed   z:19;                            // z coord (3rd loc value)
-         signed   deltaX:13;                       // change in x
+         signed   x:19;                            // x coord (1st loc value)
+         signed   deltaHeading:10;                 // change in heading
+         unsigned padding02:3;
 /*0016*/
-         signed   deltaZ:13;                       // change in z
+         unsigned heading:12;                      // heading
          signed   y:19;                            // y coord (2nd loc value)
+         unsigned padding03:1;
 /*0020*/
-         unsigned pitch:12;                        // pitch (up/down heading)
-         signed   deltaY:13;                       // change in y
-         unsigned padding04:7;
+         signed   deltaX:13;                       // change in x
+         signed   deltaZ:13;                       // change in z
+         unsigned padding04:6;
 /*0024*/
 };
 
 /*
 ** Self Position Update
-** Length: 42 Octets
+** Length: 38 Octets
 ** OpCode: PlayerPosCode
 */
 struct playerSelfPosStruct
@@ -2419,28 +2424,25 @@ struct playerSelfPosStruct
 /*0002*/ uint16_t spawnId;                       // Player's spawn id
 /*0004*/ uint16_t unknown0004;                   // ***Placeholder
 /*0006*/
-	 float    z;                         // z coord (3rd loc value)
+	 unsigned pitch:12;                  // pitch (up/down heading)
+	 signed   deltaHeading:10;           // change in heading
+	 signed   animation:10;              // current animation
 /*0010*/
-	 float    deltaY;                    // change in y
+	 float    y;                         // y coord (2nd loc value)
 /*0014*/
 	 float    deltaZ;                    // change in z
 /*0018*/
-	 float    y;                         // y coord (2nd loc value)
+	 float    deltaY;                    // change in y
 /*0022*/
-	 signed   animation:10;              // current animation
-	 unsigned padding04:22;
+	 float    z;                         // z coord (3rd loc value)
 /*0026*/
-	 float    x;                         // x coord (1st loc value)
-/*0030*/
-	 unsigned heading:12;                // heading
-	 unsigned padding06:20;
-/*0034*/
 	 float    deltaX;                    // change in x
+/*0030*/
+	 float    x;                         // x coord (1st loc value)
+/*0034*/
+	 unsigned heading:12;                // heading
+	 unsigned padding07:20;
 /*0038*/
-	 unsigned pitch:12;                  // pitch (up/down heading)
-	 signed   deltaHeading:10;           // change in heading
-	 unsigned padding08:10;
-/*0042*/
 };
 
 /*
